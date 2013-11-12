0871b4900fa5154834381b6e20eef75324dfcd30
huyettcp <huyettcp@gmail.com>
2013-11-12 11:48:49 -0500

    Players graph looking good

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,19 @@
 class PlayersController < ApplicationController
   def index 
-    @players = Player.order("salary DESC")
+    
+    @players = Player.where("team_id = 349").order("salary DESC") 
+    respond_to do |format|
+      format.html
+      format.json {render :json => @players}
+    end
+  end
+
+  def sorted
+    @players = Player.where("team_id = 349").order("cost_performance_score DESC")
     respond_to do |format|
       format.html
       format.json {render :json => @players}
     end
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Project3::Application.routes.draw do
   root :to => 'teams#index'
   get '/teams', to: 'teams#index'
   get '/players', to: 'players#index'
-  get '/simple', to: 'teams#dead_simple'
+  get '/players_sorted', to: 'players#sorted'
 end
